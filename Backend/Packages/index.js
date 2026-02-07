83dd7e1e720240de04bc5d6a96c79f57429953a6
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-02-07 23:44:43 +0530

    understanding of packages and requiring them using catme package

--- Backend/Packages/index.js
+++ Backend/Packages/index.js
@@ -1,3 +1,8 @@
 const catme = require('cat-me');
 
 console.log(catme());
+
+//cat-me is a package that generates random cat images and facts.
+// It can be used to add some fun and entertainment to your projects.
+// You can install it using npm by running the command "npm install cat-me".
+// Once installed, you can require it in your code and call the function to get a random cat image or fact.
