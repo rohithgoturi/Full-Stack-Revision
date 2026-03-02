c350d812f9fc735bc3b141a134b2d5c7218a001c
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-03-02 10:48:20 +0530

    backend done

--- Backend/spotify-project/src/controllers/music.controller.js
+++ Backend/spotify-project/src/controllers/music.controller.js
@@ -12,31 +12,14 @@ const createMusic = async (req, res) => {
     const { title } = req.body;
     const token = req.cookies.token;
 
-    if (!token) {
-        return res.status(401).json({ message: "Unauthorized" });
-    }
-
-    let decoded;
-
-    try {
-        decoded = jwt.verify(token, JWT_SECRET);
-    } catch (err) {
-        return res.status(401).json({ message: "Invalid token" });
-    }
-
-    if (decoded.role !== "artist") {
-        return res.status(403).json({
-            message: "Only artists can upload music"
-        });
-    }
-
-    try {
+    
+    
         const result = await uploadFile(req.file.buffer);
 
         const music = await musicModel.create({
             uri: result.url,
             title,
-            artist: decoded.id
+            artist: req.user.id
         });
 
         return res.status(201).json({
@@ -44,48 +27,22 @@ const createMusic = async (req, res) => {
             music
         });
 
-    } catch (err) {
-        return res.status(500).json({
-            message: err.message
-        });
-    }
+    
 
-    console.log(decoded);
+    
 };
 
 
 const createAlbum = async (req, res) => {
 
-    const token = req.cookies.token;
-
-    if(!token){
-        return res.status(401).json({
-            message : "Unauthorized"
-        })
-    }
-
-    let decoded;
-
-    try {
-        decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-        if(decoded.role !== "artist"){
-            return res.status(403).json({
-                message : "Only artists can create albums"
-            })
-        }
-    } catch (err) {
-        return res.status(401).json({
-            message : "Invalid token"
-        })
-    }
+   
 
     const {title, musics} = req.body;
 
     const album = await albumModel.create({
         title,
         musics : musics,
-        artist : decoded.id
+        artist : req.user.id
     })
 
     res.status(201).json({
